dafc97b5b073a24193e1ce0ac804f610c82b4ce8
k-i-o <sradici26@gmail.com>
2024-09-23 01:08:15 +0200

    new logo

--- src/renderer/src/utils.ts
+++ src/renderer/src/utils.ts
@@ -19,6 +19,7 @@ export function rgbToHex(color: string) {
 
 export function adjustAlpha(color: string, alpha: number): string {
     const rgb = hexToRgb(color);
+    console.log(rgb);
     return `rgba(${rgb.r}, ${rgb.g}, ${rgb.b}, ${alpha})`;
 }
 
@@ -34,11 +35,21 @@ export function darken(color: string, percent: number): string {
     return `rgb(${darkenValue(rgb.r)}, ${darkenValue(rgb.g)}, ${darkenValue(rgb.b)})`;
 }
 
-function hexToRgb(hex: string): {r: number, g: number, b: number} {
-    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16)
-    } : {r: 0, g: 0, b: 0};
+function hexToRgb(input: string): { r: number, g: number, b: number } {
+    const rgbMatch = input.match(/^rgb\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*\)$/i);
+    if (rgbMatch) {
+        return {
+            r: parseInt(rgbMatch[1], 10),
+            g: parseInt(rgbMatch[2], 10),
+            b: parseInt(rgbMatch[3], 10)
+        };
+    }
+
+    const hexMatch = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(input);
+    return hexMatch ? {
+        r: parseInt(hexMatch[1], 16),
+        g: parseInt(hexMatch[2], 16),
+        b: parseInt(hexMatch[3], 16)
+    } : { r: 0, g: 0, b: 0 };
 }
+
